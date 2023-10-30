9965b2f529f91c0c2eca1fddcb4a22232f0b3bcc
Phillip Malboeuf <phil@phils.computer>
2023-10-30 11:55:59 -0400

    captions

--- src/lib/components/Media.svelte
+++ src/lib/components/Media.svelte
@@ -6,14 +6,30 @@
   export let webp: boolean = false
 </script>
 
-<style>
+<style lang="scss">
   img {
     display: block;
     width: 100%;
   }
+
+  picture.captioned {
+    position: relative;
+    display: block;
+
+    figcaption {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+
+      color: $banana;
+      font-size: $base / $scale;
+      padding: $base / $scale / 2 $base / $scale;
+      text-shadow: 0 0 5px $black;
+    }
+  }
 </style>
 
-<picture>
+<picture class:captioned={media?.fields.description}>
   {#if media?.fields.file}
   {#if small}
   <source srcSet="{media.fields.file.url}?w=400{webp ? '&fm=webp' : ''}" media="(max-width: 900px)" />
@@ -24,5 +40,8 @@
   <source srcSet="{media.fields.file.url}?w=1200{webp ? '&fm=webp' : ''}" media="(max-width: 1200px)" />
   <img src="{media.fields.file.url}?w=1800{webp ? '&fm=webp' : ''}" alt="{media.fields.title} {media.fields.description}" />
   {/if}
+  {/if}
+  {#if media?.fields.description}
+  <figcaption>{media.fields.description}</figcaption>
   {/if}
 </picture>
