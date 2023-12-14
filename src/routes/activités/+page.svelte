5292030a1f63c3443d2dc71d3079e50c67cd8d49
Phillip Malboeuf <phil@phils.computer>
2023-12-14 12:08:58 -0500

    tags fix

--- src/routes/activités/+page.svelte
+++ src/routes/activités/+page.svelte
@@ -30,7 +30,7 @@
         <div>
           <h3>{event.fields.titre}</h3>
           <p>{event.fields.excerpt}</p>
-          {event.fields.tags.join(' ')}
+          {event.fields.tags?.join(' ')}
         </div>
 
         <svg width="37" height="34" viewBox="0 0 37 34" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 0.00153056L14.8277 2.96255L27.3836 14.6997L0.00114253 14.7018L0.00146897 18.9015L27.384 18.8993L14.8299 30.6384L18.0026 33.5989L36.0013 16.7988L18 0.00153056Z" fill="currentColor"/></svg>
@@ -50,7 +50,7 @@
         <div>
           <h3>{event.fields.titre}</h3>
           <p>{event.fields.excerpt}</p>
-          {event.fields.tags.join(' ')}
+          {event.fields.tags?.join(' ')}
         </div>
 
         <svg width="37" height="34" viewBox="0 0 37 34" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 0.00153056L14.8277 2.96255L27.3836 14.6997L0.00114253 14.7018L0.00146897 18.9015L27.384 18.8993L14.8299 30.6384L18.0026 33.5989L36.0013 16.7988L18 0.00153056Z" fill="currentColor"/></svg>
